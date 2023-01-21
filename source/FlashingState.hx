16a48b8a632e995a8807d029174d9eb0a61a9b4b
PicoBf <98202327+PicoBf@users.noreply.github.com>
2023-01-21 13:56:02 +0800

    Add files via upload

--- source/FlashingState.hx
+++ source/FlashingState.hx
@@ -24,18 +24,18 @@ class FlashingState extends MusicBeatState
 		add(bg);
 
 		warnText = new FlxText(0, 0, FlxG.width,
-			"嘿！看这里！\n
-			这个模组由GOP团队制作！\n
-			Hey!Watch out!\n
-			This mod is made by GOP Team!\n
-			按下A继续\n
-			Press A to continue",
+			"Hey, watch out!\n
+			This Mod contains some flashing lights!\n
+			Press ENTER to disable them now or go to Options Menu.\n
+			Press ESCAPE to ignore this message.\n
+			You've been warned!",
 			32);
-		warnText.setFormat(Paths.font("中文.ttf"), 32, FlxColor.WHITE, CENTER);
+		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
 		warnText.screenCenter(Y);
 		add(warnText);
+
                 #if android
-	        addVirtualPad(NONE, A);
+	        addVirtualPad(NONE, A_B);
                 #end
 	}
 
@@ -48,19 +48,19 @@ class FlashingState extends MusicBeatState
 				FlxTransitionableState.skipNextTransIn = true;
 				FlxTransitionableState.skipNextTransOut = true;
 				if(!back) {
-					ClientPrefs.flashing = true;
+					ClientPrefs.flashing = false;
 					ClientPrefs.saveSettings();
 					FlxG.sound.play(Paths.sound('confirmMenu'));
 					FlxFlicker.flicker(warnText, 1, 0.1, false, true, function(flk:FlxFlicker) {
 						new FlxTimer().start(0.5, function (tmr:FlxTimer) {
-							MusicBeatState.switchState(new FriendsState());
+							MusicBeatState.switchState(new TitleState());
 						});
 					});
 				} else {
 					FlxG.sound.play(Paths.sound('cancelMenu'));
 					FlxTween.tween(warnText, {alpha: 0}, 1, {
 						onComplete: function (twn:FlxTween) {
-							MusicBeatState.switchState(new FriendsState());
+							MusicBeatState.switchState(new TitleState());
 						}
 					});
 				}
